f50bb13b36f5d73e2db307106c7d6adbf783ef69
Your GitHub Username <davilama@unlv.nevada.edu>
2024-05-14 00:29:53 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,20 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
 })
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    res.send(Bread[req.params.arrayIndex])// Bread[1] 
   })
   
+
 module.exports = breads
